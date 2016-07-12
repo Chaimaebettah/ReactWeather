255939014f76f668f04b008c44f32898f89c9052
Chaimaebettah <bettah.chaimae@gmail.com>
2016-07-12 13:28:58 -0400

    modify

--- public/components/About.jsx
+++ public/components/About.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 
 var About = (props)=> {
   return (
-    <h3>this is the About </h3>
+    <div>
+      <h3>this is the About </h3>
+      <p> hey its working </p>
+    </div>
   )
 };
 
