374b94b922ed3ef0695c81718b767745b770d499
Chaimaebettah <bettah.chaimae@gmail.com>
2016-07-12 15:43:18 -0400

    prep

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
+
 var express = require('express');
 
+// Create our app
 var app = express();
-var PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
+app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
+
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log("the server is Up on 4000", +PORT)
+  console.log('Express server is up on port ' + PORT);
 });
