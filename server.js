6798449698cfc66111008f4fdc1f310b00146aeb
Chaimaebettah <bettah.chaimae@gmail.com>
2016-07-12 15:13:45 -0400

    add the heroku

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT;
 
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' +req.hostname+ req.url);
+  }
+});
 app.use(express.static('public'));
 
-app.listen(4000,function(){
-  console.log("the server is Up on 4000")
+app.listen(PORT, function () {
+  console.log("the server is Up on 4000", +PORT)
 });
