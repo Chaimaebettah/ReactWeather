c35da54d7b0e0d8fc21d0f6012bed43348747cbf
Chaimaebettah <bettah.chaimae@gmail.com>
2016-07-13 09:25:36 -0400

    message

--- server.js
+++ server.js
@@ -12,11 +12,10 @@ app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
   }
-
   next();
 });
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
