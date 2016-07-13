210c50625ebf85ec11e78f61ac7590c210796c67
Chaimaebettah <bettah.chaimae@gmail.com>
2016-07-13 09:18:26 -0400

    message

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+//app.enable('trust proxy');
+
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  console.log(req.secure);
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
   }
+
+  next();
 });
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
