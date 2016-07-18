01d99a43c3a1bc3ce3870abbcbefb6c32874f75e
Chaimaebettah <bettah.chaimae@gmail.com>
2016-07-18 14:40:53 -0400

    styling the nav bar

--- public/components/Main.jsx
+++ public/components/Main.jsx
@@ -5,7 +5,6 @@ var Nav = require('Nav');
 var Main = (props) => {
   return (
     <div>
-      <h2>this is the Main </h2>
       <Nav/>
       {props.children}
     </div>
--- public/components/Nav.jsx
+++ public/components/Nav.jsx
@@ -1,15 +1,39 @@
 var React = require('react');
 var {Link,IndexLink}= require('react-router');
 
-var Nav = (props) => {
-    return (
-      <div>
-        <h3>this is the nav </h3>
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather </IndexLink>
-        <Link to="/About" activeClassName="active" activeStyle={{fontWeight:'bold'}}> About</Link>
-        <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}> Examples</Link>
-      </div>
-    )
-};
+var Nav = React.createClass({
+    onSearch:function(e){
+        e.preventDefault();
+        alert('Need some work ');
+    },
+    render:function(){
+        return (
+          <div className="top-bar">
+              <div className="top-bar-left">
+                  <ul className="menu">
+                      <li className="menu-text">Navigation </li>
+                      <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather </IndexLink></li>
+                      <li><Link to="/About" activeClassName="active" activeStyle={{fontWeight:'bold'}}> About</Link></li>
+                      <li><Link to="/Examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}> Examples</Link></li>
+                  </ul>
+              </div>
+              <div  className="top-bar-right">
+                  <form  onSubmit={this.onSearch}>
+                      <ul className="menu">
+                      <li><input type="search" placeholder="Search" /></li>
+                      <li><button type="button" className="button">Search</button></li>
+                      </ul>
+
+                  </form>
+              </div>
+          </div>
+
+        )
+    }
+});
+
 
 module.exports = Nav;
+
+
+
